5dc69233dc16bc9661646c8ddaadde0f0ba18a06
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-23 05:54:02 +0700

    first commit

--- driver/boot/keygen.sh
+++ driver/boot/keygen.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker compose --env-file .env -f server/keygen/docker-compose.yml up -d
+docker compose --env-file .env -f server/keygen/docker-compose.yml up -d --build --force-recreate
 
 sudo chown 1000:1000 -R server/keygen && sudo chmod 777 -R server/keygen
 
