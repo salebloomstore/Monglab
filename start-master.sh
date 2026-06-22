04a626a6be48085df592229baa0ac625a7f7e270
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-22 22:24:05 +0700

    first commit

--- start-master.sh
+++ start-master.sh
@@ -4,6 +4,32 @@ set -e
 
 find . -type f -name "*.sh" -exec chmod +x {} \;
 
+SOURCE="/keygen/keyfile/mongo-keyfile"
+
+for DIR in \
+    /mongos-router-dn \
+    /mongos-router-hn \
+    /mongos-router-sg \
+    /sharde-everes-dn \
+    /sharde-everes-hn \
+    /sharde-everes-sg \
+    /sharde-fansip-dn \
+    /sharde-fansip-hn \
+    /sharde-fansip-sg \
+    /sharde-fujimo-dn \
+    /sharde-fujimo-hn \
+    /sharde-fujimo-sg \
+    /sharde-matter-dn \
+    /sharde-matter-hn \
+    /sharde-matter-sg
+do
+    TARGET="$DIR/keyfile/mongo-keyfile"
+
+    cp "$SOURCE" "$TARGET"
+    chown 999:999 "$TARGET"
+    chmod 400 "$TARGET"
+done
+
 # docker compose down -v --remove-orphans
 # docker compose up -d --build --force-recreate
 
