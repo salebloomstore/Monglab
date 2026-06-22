d22b1ba411d1dbfceec46f1ac31bd23e4acc27b2
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-23 02:45:43 +0700

    first commit

--- driver/shutdown/master.sh
+++ driver/shutdown/master.sh
@@ -4,16 +4,16 @@ set -e
 
 find . -type f -name "*.sh" -exec chmod +x {} \;
 
-chmod +x sharde-everes-dn.sh && sudo ./sharde-everes-dn.sh
-chmod +x sharde-everes-hn.sh && sudo ./sharde-everes-hn.sh
-chmod +x sharde-everes-sg.sh && sudo ./sharde-everes-sg.sh
+chmod +x server/sharde-everes-dn.sh && sudo ./server/sharde-everes-dn.sh
+chmod +x server/sharde-everes-hn.sh && sudo ./server/sharde-everes-hn.sh
+chmod +x server/sharde-everes-sg.sh && sudo ./server/sharde-everes-sg.sh
 
-chmod +x sharde-fansip-dn.sh && sudo ./sharde-fansip-dn.sh
-chmod +x sharde-fansip-hn.sh && sudo ./sharde-fansip-hn.sh
-chmod +x sharde-fansip-sg.sh && sudo ./sharde-fansip-sg.sh
+chmod +x server/sharde-fansip-dn.sh && sudo ./server/sharde-fansip-dn.sh
+chmod +x server/sharde-fansip-hn.sh && sudo ./server/sharde-fansip-hn.sh
+chmod +x server/sharde-fansip-sg.sh && sudo ./server/sharde-fansip-sg.sh
 
-chmod +x sharde-fujimo-dn.sh && sudo ./sharde-fujimo-dn.sh
-chmod +x sharde-fujimo-hn.sh && sudo ./sharde-fujimo-hn.sh
-chmod +x sharde-fujimo-sg.sh && sudo ./sharde-fujimo-sg.sh
+chmod +x server/sharde-fujimo-dn.sh && sudo ./server/sharde-fujimo-dn.sh
+chmod +x server/sharde-fujimo-hn.sh && sudo ./server/sharde-fujimo-hn.sh
+chmod +x server/sharde-fujimo-sg.sh && sudo ./server/sharde-fujimo-sg.sh
 
 echo "Master downed successfully!"
