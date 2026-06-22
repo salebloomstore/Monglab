5af0e9811af0f826fe2c58f6a0fba6bc5d5d8d77
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-23 00:40:50 +0700

    first commit

--- restart-sharde-fujimo-dn.sh
+++ restart-sharde-fujimo-dn.sh
@@ -3,6 +3,7 @@
 set -e
 
 docker compose --env-file .env -f sharde-fujimo-dn/docker-compose.yml down -v --remove-orphans
+docker ps -aq --filter name=sharde-fujimo-dn | xargs -r docker rm -f
 docker compose --env-file .env -f sharde-fujimo-dn/docker-compose.yml up -d --build --force-recreate
 
 echo "sharde-fujimo-dn restarted successfully!"
--- restart-sharde-fujimo-sg.sh
+++ restart-sharde-fujimo-sg.sh
@@ -3,6 +3,7 @@
 set -e
 
 docker compose --env-file .env -f sharde-fujimo-sg/docker-compose.yml down -v --remove-orphans
+docker ps -aq --filter name=sharde-fujimo-sg | xargs -r docker rm -f
 docker compose --env-file .env -f sharde-fujimo-sg/docker-compose.yml up -d --build --force-recreate
 
 echo "sharde-fujimo-sg restarted successfully!"
