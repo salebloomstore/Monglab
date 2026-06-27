3ad24a0029b1d07b695e43cf78ff75230c5695a6
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-27 11:13:00 +0700

    first commit

---
+++ server/sharde-everes-dn/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+echo 'WAITING FOR KEYFILE...'
+while [ ! -f /etc/mongo-keyfile ]; do sleep 2; done
+echo 'KEYFILE CREATED'
+
+mkdir -p /data/db
+
+echo 'STARTING UP SHARDE-EVERES-DN IN PROGRESS WITH NO AUTH'
+mongod --replSet everesRS --bind_ip_all --port ${SHARDE_EVERES_DN_PORT_OF} --dbpath /data/db --fork --logpath /tmp/mongod.log --shardsvr
+echo 'THE SHARDE-EVERES-DN HAVE FINISHED BOOTING UP WITH NO AUTH'
+
+echo 'WAITING FOR SHARDE-EVERES-HN'
+until mongosh --quiet --host ${SHARDE_EVERES_HN_ADRS}:${SHARDE_EVERES_HN_PORT_OF} --eval 'db.adminCommand({ ping: 1 })' > /dev/null 2>&1; do sleep 2; done
+echo 'SHARDE-EVERES-HN READY'
+
+echo 'WAITING FOR SHARDE-EVERES-SG'
+until mongosh --quiet --host ${SHARDE_EVERES_SG_ADRS}:${SHARDE_EVERES_SG_PORT_OF} --eval 'db.adminCommand({ ping: 1 })' > /dev/null 2>&1; do sleep 2; done
+echo 'SHARDE-EVERES-SG READY'
+
+echo 'WAITING SHARDE-EVERES-DN FOR INIT'
+mongosh --host ${SHARDE_EVERES_DN_ADRS}:${SHARDE_EVERES_DN_PORT_OF} /init/init-sharde-everes.js
+echo 'SHARDE-EVERES-DN INIT DONE'
+
+echo 'WAITING FOR STARTING UP SHARDE-EVERES-DN IN PROGRESS WITH AUTH'
+mongod --shutdown --dbpath /data/db
+while pgrep mongod >/dev/null; do sleep 2; done
+mongod --replSet everesRS --bind_ip_all --port ${SHARDE_EVERES_DN_PORT_OF} --dbpath /data/db --auth --keyFile /etc/mongo-keyfile --setParameter enableLocalhostAuthBypass=0 --fork --logpath /tmp/mongod.log --shardsvr
+until mongosh --quiet --host ${SHARDE_EVERES_DN_ADRS}:${SHARDE_EVERES_DN_PORT_OF} --eval 'db.adminCommand({ ping: 1 })' > /dev/null 2>&1; do sleep 2; done
+echo 'THE SHARDE-EVERES-DN HAVE FINISHED BOOTING UP WITH AUTH'
+
+tail -f /dev/null
