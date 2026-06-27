4118761cf3a07ef610dc1414eb13295287777430
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-27 22:08:35 +0700

    first commit

--- server/mongos-expres-io/entrypoint.sh
+++ server/mongos-expres-io/entrypoint.sh
@@ -2,9 +2,9 @@
 
 echo 'Waiting for mongos startup...'
 
-until nc -z \"$${MONGOS_ROUTER_DN_ADRS}\" \"$${MONGOS_ROUTER_DN_PORT_OF}\"; do sleep 2; done
-until nc -z \"$${MONGOS_ROUTER_HN_ADRS}\" \"$${MONGOS_ROUTER_HN_PORT_OF}\"; do sleep 2; done
-until nc -z \"$${MONGOS_ROUTER_SG_ADRS}\" \"$${MONGOS_ROUTER_SG_PORT_OF}\"; do sleep 2; done
+until nc -z ${MONGOS_ROUTER_DN_ADRS} ${MONGOS_ROUTER_DN_PORT_OF}; do sleep 2; done
+until nc -z ${MONGOS_ROUTER_HN_ADRS} ${MONGOS_ROUTER_HN_PORT_OF}; do sleep 2; done
+until nc -z ${MONGOS_ROUTER_SG_ADRS} ${MONGOS_ROUTER_SG_PORT_OF}; do sleep 2; done
 
 echo 'mongos is ready'
 
